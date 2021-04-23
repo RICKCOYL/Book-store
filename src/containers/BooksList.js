f8d7d14a244e6fbeede3fbd23ecc4c9db9470ab3
Morrism1 <morrismontana0@gmail.com>
2021-04-23 20:53:54 +0200

    Create booklist component inside the container directory

---
+++ src/containers/BooksList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function BooksList() {
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>BookId</th>
+          <th>title</th>
+          <th>category</th>
+        </tr>
+      </table>
+    </div>
+  );
+}
