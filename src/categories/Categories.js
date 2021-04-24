7d9bc67859e4410918f4b74d7cb8dae6bae04897
RICKCOYL <rickoburu@gmail.com>
2021-04-24 18:07:05 +0300

    Add book form

---
+++ src/categories/Categories.js
@@ -0,0 +1,4 @@
+const categories = ['Action', 'Biography', 'History',
+  'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+
+export default categories;
--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import categories from '../categories/Categories';
 
 export default function BooksForm() {
   return (
     <div>
-      BooksForm
+      <h3>Add New Book</h3>
+      <form>
+        <div>
+          <span>Book Name: </span>
+          <br />
+          <input type="text" name="title" id="title" placeholder="enter book name" />
+        </div>
+        <div>
+          <span>Book Category: </span>
+          <br />
+          <select name="category" id="category">
+            {categories.map((category) => (
+              <option key={`${category}`} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
