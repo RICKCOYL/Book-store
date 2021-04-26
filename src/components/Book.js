2e2983eed4a37dc113fb02d142ee6f16a92421a9
Morrism1 <morrismontana0@gmail.com>
2021-04-27 00:31:47 +0200

    Add handleremove func to the onclick to remove a book

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Book({ book }) {
-  const { id, title, category } = book;
+export default function Book({ book, handleRemove }) {
+  const {
+    id, title, category,
+  } = book;
 
   return (
     <tr>
       <td>{id}</td>
       <td>{title}</td>
       <td>{category}</td>
+      <td><button type="button" onClick={(e) => handleRemove(e, book)}>Remove Book</button></td>
     </tr>
   );
 }
@@ -22,5 +25,7 @@ Book.propTypes = {
     id: PropTypes.string,
     title: PropTypes.string,
     category: PropTypes.string,
+
   }),
+  handleRemove: PropTypes.func.isRequired,
 };
