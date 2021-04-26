b45341d413adcd41b030e49460f831062966df6c
Morrism1 <morrismontana0@gmail.com>
2021-04-26 09:14:15 +0200

    Refactor the index.js initial state object to return books and their id to be strings

--- src/index.js
+++ src/index.js
@@ -7,9 +7,29 @@ import App from './components/App';
 import reducers from './reducers/index';
 // import { books } from './categories/Categories';
 
-const initialState = { Book1: { id: Math.random, title: 'The Savior’s Champion', category: 'Fantasy' }, Book2: { id: Math.random, title: 'The Hobbit', category: 'Adventure' }, Book3: { id: Math.random, title: 'Romeo and Juliet', category: 'Classic' } };
+const initialState = {
+  books: [
+    {
+      id: Math.floor(Math.random() * 11).toString(),
+      title: 'The Savior’s Champion',
+      category: 'Fantasy',
+    },
+    {
+      id: (Math.floor(Math.random() * 11)).toString(),
+      title: 'The Hobbit',
+      category: 'Adventure',
+    },
+    {
+      id: (Math.floor(Math.random() * 11)).toString(),
+      title: 'Romeo and Juliet',
+      category: 'Classic',
+    },
+  ],
+};
 
 const store = createStore(reducers, initialState);
+// eslint-disable-next-line no-console
+console.log(store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
