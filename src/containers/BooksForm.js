33f17fad1e5c299f5bef59ccc6a0e9ce7425fc55
RICKCOYL <rickoburu@gmail.com>
2021-04-26 16:48:42 +0300

    Add handlechange function

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,20 +1,42 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-console */
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import { categories } from '../categories/Categories';
+import { CREATE_BOOK } from '../Actions';
 
-export default function BooksForm() {
+function BooksForm() {
+  const [values, setValues] = useState({
+    title: '',
+    category: 'Action',
+  });
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    e.persist();
+    setValues((values) => ({
+      ...values,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = () => {
+    const { title, category } = values;
+  };
+
   return (
     <div>
       <h3>Add New Book</h3>
       <form>
         <div>
-          <span>Name: </span>
+          <span>Title: </span>
           <br />
-          <input type="text" name="title" id="title" placeholder="enter book name" />
+          <input type="text" name="title" id="title" placeholder="enter book name" value={values.title} onChange={handleChange} />
         </div>
         <div>
           <span>Category: </span>
           <br />
-          <select name="category" id="category">
+          <select name="category" id="category" value={values.category} onChange={handleChange}>
             {categories.map((category) => (
               <option key={`${category}`} value={category}>
                 {category}
@@ -22,8 +44,14 @@ export default function BooksForm() {
             ))}
           </select>
         </div>
-        <button type="submit">Submit</button>
+        <button onSubmit={handleSubmit} type="submit">Submit</button>
       </form>
     </div>
   );
 }
+
+const mapDispatchToProps = (dispatch) => ({
+  createBook: () => dispatch({ type: CREATE_BOOK }),
+});
+
+export default connect(null, mapDispatchToProps)(BooksForm);
