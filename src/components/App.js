8503e4edc9302c9341e932d2a83f35923821657b
Morrism1 <morrismontana0@gmail.com>
2021-04-27 14:37:39 +0200

    Removed the CategoryFilter component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
 import BooksForm from '../containers/BooksForm';
 import BooksList from '../containers/BooksList';
-import CategoryFilter from './CategoryFilter';
 
 function App() {
   return (
     <div>
-      <CategoryFilter />
       <BooksForm />
       <BooksList />
     </div>
