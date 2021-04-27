dba1955f847b1ddc5158e18b1c958e1a2dab228c
RICKCOYL <rickoburu@gmail.com>
2021-04-27 16:15:57 +0300

    Add nav bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import BooksForm from '../containers/BooksForm';
 import BooksList from '../containers/BooksList';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div>
+      <Navbar />
       <BooksForm />
       <BooksList />
     </div>
