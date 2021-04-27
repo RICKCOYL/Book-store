338d8e6d69479681066a51567853d9c477b11a89
Morrism1 <morrismontana0@gmail.com>
2021-04-28 01:13:39 +0200

    Add navbar component and add container

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
+import Container from 'react-bootstrap/Container';
 import BooksForm from '../containers/BooksForm';
 import BooksList from '../containers/BooksList';
-import Navbar from './Navbar';
+import NavBar from './Navbar';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <BooksList />
-      <BooksForm />
+    <div className="bg-light mh-100">
+      <NavBar />
+      <Container>
+        <BooksList />
+        <BooksForm />
+      </Container>
     </div>
   );
 }
